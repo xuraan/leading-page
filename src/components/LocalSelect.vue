cc02810859e535ef26e095c4f9330797eacca54a
afrcvn <afrcvn@gmail.com>
2023-03-31 04:20:50 -0400

    On branch main
    Initial commit
    
    Changes to be committed:
    	new file:   src/assets/main.css
    	new file:   src/components/LocalSelect.vue
    
    Untracked files:
    	.eslintrc.cjs
    	.gitignore
    	.prettierrc.json
    	.vscode/
    	README.md
    	e2e/
    	env.d.ts
    	index.html
    	package-lock.json
    	package.json
    	playwright.config.ts
    	public/
    	src/App.vue
    	src/assets/base.css
    	src/assets/img/
    	src/assets/logo.svg
    	src/components/AccordionItem.vue
    	src/components/Faqs.vue
    	src/components/Feature.vue
    	src/components/Features.vue
    	src/components/HelloWorld.vue
    	src/components/Hero.vue
    	src/components/Navbar.vue
    	src/components/Section.vue
    	src/components/TheWelcome.vue
    	src/components/Topbar.vue
    	src/components/Version.vue
    	src/components/Versions.vue
    	src/components/WelcomeItem.vue
    	src/components/__tests__/
    	src/components/icons/
    	src/helper/
    	src/main.ts
    	src/router/
    	src/stores/
    	src/views/
    	tsconfig.app.json
    	tsconfig.json
    	tsconfig.node.json
    	tsconfig.vitest.json
    	vite.config.ts
    	vitest.config.ts

---
+++ src/components/LocalSelect.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="input-group mb-3 select  ">
+        <span class="input-group-text  glass" id="basic-addon1">
+            <i class="bi bi-globe"></i>
+        </span>
+        <!-- <select id="language-select" :value="store.local" @change="store.updateLocal($event.target?.value)">
+      </select> -->
+        <select v-model="store.local" class="form-select form-select-sm glass btn-p" aria-label=".form-select-sm example">
+            <option v-for="local in store.locals" :value="local.id">
+                {{ local.name }} {{ local.flag }}
+            </option>
+        </select>
+    </div>
+</template>
+  
+<script setup lang="ts">
+import { useLocalStore } from '@/stores/local';
+
+const store = useLocalStore()
+
+console.log(6)
+console.log(store.local?.id)
+
+</script>
+
+
+
+
+
+<style scoped>
+    .select {
+       width: 170px;
+       padding-top: 25px;;
+    }
+</style>
